81b7ef2a3da320c1889a3bf5196effe758be46dd
Michal Ilyayev <michal.ilyayev@gmail.com>
2024-08-07 20:53:38 -0400

    integration with scss and stylelint

---
+++ .stylelintrc.mjs
@@ -0,0 +1,74 @@
+export default {
+  extends: [
+    'stylelint-config-standard-scss'
+  ],
+  ignoreFiles: [
+    'node_modules/*'
+  ],
+  rules: {
+    'color-hex-length': null,
+    'color-function-notation': null,
+    'alpha-value-notation': 'number',
+    'font-family-no-missing-generic-family-keyword': null,
+    'no-descending-specificity': null,
+    'block-no-empty': null,
+    'no-duplicate-selectors': null,
+    'font-family-no-duplicate-names': null,
+    'shorthand-property-no-redundant-values': null,
+    'custom-property-empty-line-before': null,
+    'scss/double-slash-comment-whitespace-inside': null,
+    'scss/at-mixin-argumentless-call-parentheses': null,
+    'selector-class-pattern': null,
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignoreTypes: [
+          '/^ion-/',
+          '/^tdf-/'
+        ]
+      }
+    ],
+    'declaration-empty-line-before': [
+      'always',
+      {
+        ignore: [
+          'after-comment',
+          'after-declaration',
+          'first-nested',
+          'inside-single-line-block'
+        ]
+      }
+    ],
+    'declaration-block-no-redundant-longhand-properties': [
+      true,
+      {
+        ignoreShorthands: [
+          '/flex/'
+        ]
+      }
+    ],
+    'value-keyword-case': [
+      'lower',
+      {
+        ignoreProperties: [
+          '/font/'
+        ],
+        camelCaseSvgKeywords: true
+      }
+    ],
+
+    // Tailwind CSS
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen'
+        ]
+      }
+    ]
+  }
+}
